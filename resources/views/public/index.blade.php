c13d5e03eb8afb126120914d3f7bb831b6172ab4
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-04-10 19:37:18 -0300

    feat/criando e adicionando header no index publico

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,1 +1,16 @@
-Olá
+<!DOCTYPE html>
+<html class="bg-slate-900">
+<head>
+    <title>HVUNIBAVE</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+@include('public.util.header')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+@vite('resources/js/chartTest.js')
+@vite('resources/js/chartTest1.js')
+@vite('resources/js/chartTest2.js')
+@vite('node_modules/flowbite/dist/flowbite.min.js')
+@vite('node_modules/chart.js/dist/chart.js')
+</body>
+</html>
---
+++ resources/views/public/util/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="{{ url('') }}" class="flex items-center">
+                <img src="{{asset('user.png')}}" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">HVUNIBAVE</span>
+            </a>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
+                <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm">
+                    <li>
+                        <a href="{{ url('') }}" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 border-b border-gray-100" aria-current="page">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
