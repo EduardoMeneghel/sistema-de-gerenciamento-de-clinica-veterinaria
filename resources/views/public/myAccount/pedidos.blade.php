13b3a65b51d39dcb3311e1f1ad8b76638b9b6211
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-04-28 23:45:10 -0300

    feat/adicionando abaPedidos na minha-conta

--- resources/views/public/myAccount/pedidos.blade.php
+++ resources/views/public/myAccount/pedidos.blade.php
@@ -1,5 +1,6 @@
 <section class="container mx-auto w-full bg-white" style="display: none;" id="pedidos">
-    <div class="relative overflow-x-auto">
-       <h1>pedidos</h1>
+    <div class="relative overflow-x-auto ">
+        <h1 class="text-4xl font-black text-center mt-16">Pedidos</h1>
+        <p class="my-16 text-center">Sem pedidos</p>
     </div>
 </section>
