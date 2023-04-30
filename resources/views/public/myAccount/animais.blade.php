bf155c809397cf68d1cea6e4b0c51d2cf758fbc3
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-04-29 21:09:16 -0300

    feat/criando aba de anaimais

--- resources/views/public/myAccount/animais.blade.php
+++ resources/views/public/myAccount/animais.blade.php
@@ -1,5 +1,6 @@
 <section class="container mx-auto w-full bg-white" style="display: none;" id="animais">
-    <div class="relative overflow-x-auto">
-       <h1>animais</h1>
+    <div class="relative overflow-x-auto ">
+        <h1 class="text-4xl font-black text-center mt-16">Animais</h1>
+        <p class="my-16 text-center">Sem Animais</p>
     </div>
 </section>
