ad7eb737033a26679d32154ce6e24a36215e7828
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-04-30 23:10:18 -0300

    feat/adicionando icones da pagina de minha conta

--- resources/views/public/myAccount/minhaConta.blade.php
+++ resources/views/public/myAccount/minhaConta.blade.php
@@ -9,19 +9,34 @@
         <section class="container mx-auto mt-16">
             <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
                 <li class="mr-2">
-                    <button onclick="utils.perfil()" id="abaPerfil" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Perfil</button>    
+                    <button onclick="utils.perfil()" id="abaPerfil" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">
+                        <img class="inline-block mr-1 mb-1" src="https://img.icons8.com/material-rounded/24/E5E7EB/user.png"/>
+                        Perfil
+                    </button>    
                 </li>
                 <li class="mr-2">
-                    <button onclick="utils.endereco()" id="abaEndereco" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Endereço</button>
+                    <button onclick="utils.endereco()" id="abaEndereco" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                    <img class="inline-block mr-1 mb-1" src="https://img.icons8.com/material-rounded/24/E5E7EB/address.png"/>
+                        Endereço
+                    </button>
                 </li>
                 <li class="mr-2">
-                    <button onclick="utils.pedidos()" id="abaPedidos" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Pedidos</button>
+                    <button onclick="utils.pedidos()" id="abaPedidos" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                        <img class="inline-block mr-1 mb-1" src="https://img.icons8.com/material-sharp/24/E5E7EB/online-order.png"/>   
+                        Pedidos
+                    </button>
                 </li>
                 <li class="mr-2">
-                    <button onclick="utils.animais()" id="abaAnimais" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Animais</button>
+                    <button onclick="utils.animais()" id="abaAnimais" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                        <img class="inline-block mr-1 mb-1" src="https://img.icons8.com/material-sharp/24/E5E7EB/parakeet.png"/>    
+                        Animais
+                    </button>
                 </li>
                 <li class="mr-2">
-                    <button onclick="utils.sair()" id="abaSair" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Sair</button>
+                    <button onclick="utils.sair()" id="abaSair" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">
+                        <img class="inline-block mr-1 mb-1" src="https://img.icons8.com/material-sharp/24/E5E7EB/emergency-exit.png"/>
+                        Sair
+                    </button>
                 </li>
             </ul>
         </section>
