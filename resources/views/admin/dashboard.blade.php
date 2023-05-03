b01a6a3e4675b7d0f655a94603edeb1cb86f8da1
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-05-03 00:45:57 -0300

    feat/adicionando mais exemplos da graficos para deixar o dashboard completo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,21 +7,26 @@
 <body>
 @include('admin.util.header')
 <p class="text-4xl text-white text-center my-12 font-black text-gray-900 dark:text-white">Dashboard</p>
-<section class="flex flex-nowrap container mx-auto">
-    <div class="bg-white rounded-lg shadow m-4">
+<section class="flex container mx-auto">
+    <div class="bg-white rounded-lg shadow w-full m-4">
         <canvas id="myChart"></canvas>
     </div>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
+    <div class="bg-white rounded-lg shadow w-full m-4">
         <canvas id="myChart1"></canvas>
     </div>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
+    <div class="bg-white rounded-lg shadow w-full m-4">
         <canvas id="myChart2"></canvas>
     </div>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
+    <div class="bg-white rounded-lg shadow w-full m-4">
         <canvas id="myChart3"></canvas>
     </div>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
+</section>
+<section class="flex container mx-auto">
+    <div class="bg-white rounded-lg shadow w-full m-4">
         <canvas id="myChart4"></canvas>
+    </div>
+    <div class="bg-white rounded-lg shadow w-full m-4">
+        <canvas id="myChart5"></canvas>
     </div>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -30,6 +35,7 @@
 @vite('resources/js/chartTest2.js')
 @vite('resources/js/chartTest3.js')
 @vite('resources/js/chartTest4.js')
+@vite('resources/js/chartTest5.js')
 @vite('node_modules/flowbite/dist/flowbite.min.js')
 @vite('node_modules/chart.js/dist/chart.js')
 </body>
