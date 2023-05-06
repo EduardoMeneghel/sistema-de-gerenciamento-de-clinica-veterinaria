13729374b4d7d2e13ad3a58f0f53f9665b4d82fc
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-05-06 19:20:12 -0300

    feat/alterando mensagem do rodapé

--- resources/views/public/util/footer.blade.php
+++ resources/views/public/util/footer.blade.php
@@ -1,6 +1,6 @@
 
 <footer class="bg-white dark:bg-gray-900 mt-12">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <p class="text-center">Criado Por Eduardo Meneghel</p>
+        <p class="text-center">Desenvolvido Por Eduardo Meneghel</p>
     </div>
 </footer>
