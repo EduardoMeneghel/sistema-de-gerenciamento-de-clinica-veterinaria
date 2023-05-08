dfd8e5df57103583200b43e1cdeef31c4e0d0a12
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-05-07 22:47:21 -0300

    feat/alterando listagem de categorias

--- resources/views/public/products/categories.blade.php
+++ resources/views/public/products/categories.blade.php
@@ -1,49 +1,35 @@
 <p class="text-4xl text-white text-center my-12 font-black text-gray-900 dark:text-white">Categorias</p>
-<section class="flex flex-nowrap w-4/5 mx-auto">
-    <div>
-        <a class="my-auto w-16 hover:bg-gray-700 rounded-full m-4">
-            <img src="https://img.icons8.com/ios-filled/50/FFFFFF/less-than.png"/>
-        </a>
-    </div>      
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div class="mx-4">
-        <img class="rounded-full max-w-none object-cover w-24 h-24" src="https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png" alt="image description">
-        <p class="text-white font-black mt-2">CACHORRO</p>
-    </div>
-    <div>
-        <a class="my-auto w-16 hover:bg-gray-700 rounded-full m-4">
-            <img src="https://img.icons8.com/ios-filled/50/FFFFFF/more-than.png"/>
-        </a>
-    </div>
+<section class="flex flex-nowrap w-full justify-center mx-auto"> 
+        <div class="mx-4 text-center text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/dog.png" alt="imagem de cachorro">
+            <p class="text-white font-black mt-2">CACHORRO</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/cat.png" alt="imagem de gato">
+            <p class="text-white font-black mt-2">GATO</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/bull.png" alt="imagem de boi">
+            <p class="text-white font-black mt-2">BOI</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/pig.png" alt="imagem de porco">
+            <p class="text-white font-black mt-2">PORCO</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/sheep.png" alt="imagem de ovelha">
+            <p class="text-white font-black mt-2">OVELHA</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/chicken.png" alt="imagem de galinha">
+            <p class="text-white font-black mt-2">GALINHA</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/bird.png" alt="imagem de passaro">
+            <p class="text-white font-black mt-2">PASSARO</p>
+        </div>
+        <div class="mx-4 text-center">
+            <img class="max-w-none object-cover w-24 h-24" src="https://img.icons8.com/sf-black-filled/256/FFFFFF/fish.png" alt="imagem de peixe">
+            <p class="text-white font-black mt-2">PEIXE</p>
+        </div>
 </section>
